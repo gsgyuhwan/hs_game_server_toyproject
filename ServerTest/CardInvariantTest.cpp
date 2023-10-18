9280440de784c394fb985b79f048a883c3a7d708
gsgyuhwan <isaac9796@naver.com>
2023-10-18 10:52:54 +0900

    Update CardInvariantTest.cpp

--- ServerTest/CardInvariantTest.cpp
+++ ServerTest/CardInvariantTest.cpp
@@ -1,16 +1,16 @@
 #include "pch.h"
 
 TEST(DefaultConstructorTest, CardInvariantTest) {
-	CardInvariant d=CardInvariant();
-	EXPECT_FALSE(d.after_played.has_value());
-	EXPECT_FALSE(d.when_played.has_value());
-	EXPECT_FALSE(d.when_drawed.has_value());
-	EXPECT_FALSE(d.after_drawed.has_value());
-	EXPECT_FALSE(d.battlecry.has_value());
-	EXPECT_EQ(d.default_cost, 0);
-	EXPECT_EQ(d.default_attack, 0);
-	EXPECT_EQ(d.default_h_or_d, 0);
-	EXPECT_EQ(d.card_tye, CardType::minion);
+	CardInvariant d = CardInvariant();
+	EXPECT_FALSE(d.function_features.after_played.has_value());
+	EXPECT_FALSE(d.function_features.when_played.has_value());
+	EXPECT_FALSE(d.function_features.when_drawed.has_value());
+	EXPECT_FALSE(d.function_features.after_drawed.has_value());
+	EXPECT_FALSE(d.function_features.battlecry.has_value());
+	EXPECT_EQ(d.stat_features.default_cost, 0);
+	EXPECT_EQ(d.stat_features.default_attack, 0);
+	EXPECT_EQ(d.stat_features.default_h_or_d, 0);
+	EXPECT_EQ(d.card_type, CardType::minion);
 }
 
 TEST(MinionConstructorTest, CardInvariantTest) {
@@ -18,34 +18,33 @@ TEST(MinionConstructorTest, CardInvariantTest) {
 	stat_features_t sf;
 	sf.default_attack = 1;
 	sf.default_h_or_d = 1;
-	CardInvariant m = CardInvariant(CardType::minon,ff,sf);
-	EXPECT_FALSE(m.after_played.has_value());
-	EXPECT_FALSE(m.when_played.has_value());
-	EXPECT_FALSE(m.when_drawed.has_value());
-	EXPECT_FALSE(m.after_drawed.has_value());
-	EXPECT_FALSE(m.battlecry.has_value());
-	EXPECT_EQ(m.default_cost, 0);
-	EXPECT_EQ(m.default_attack, 1);
-	EXPECT_EQ(m.default_h_or_d, 1);
+	CardInvariant m = CardInvariant(CardType::minion,ff,sf);
+	EXPECT_FALSE(m.function_features.after_played.has_value());
+	EXPECT_FALSE(m.function_features.when_played.has_value());
+	EXPECT_FALSE(m.function_features.when_drawed.has_value());
+	EXPECT_FALSE(m.function_features.after_drawed.has_value());
+	EXPECT_FALSE(m.function_features.battlecry.has_value());
+	EXPECT_EQ(m.stat_features.default_cost, 0);
+	EXPECT_EQ(m.stat_features.default_attack, 1);
+	EXPECT_EQ(m.stat_features.default_h_or_d, 1);
 	EXPECT_EQ(m.card_type, CardType::minion);
 }
 
 TEST(SpellConstructorTest, CardInvariantTest) {
 	function_features_t ff;
-	ff.when_played=
-		[&](){
-		this.GainMana(1);
-	}
+	ff.when_played =
+		[](Match& match, Move& move) mutable {
+	};
 	stat_features_t sf;
-	CardInvariant s = CardInvariant(CardType::minon, ff, sf);
-	EXPECT_FALSE(s.after_played.has_value());
-	EXPECT_TRUE(s.when_played.has_value());
-	EXPECT_FALSE(s.when_drawed.has_value());
-	EXPECT_FALSE(s.after_drawed.has_value());
-	EXPECT_FALSE(s.battlecry.has_value());
-	EXPECT_EQ(s.default_cost, 0);
-	EXPECT_EQ(s.default_attack, 0);
-	EXPECT_EQ(s.default_h_or_d, 0);
+	CardInvariant s = CardInvariant(CardType::spell, ff, sf);
+	EXPECT_FALSE(s.function_features.after_played.has_value());
+	EXPECT_TRUE(s.function_features.when_played.has_value());
+	EXPECT_FALSE(s.function_features.when_drawed.has_value());
+	EXPECT_FALSE(s.function_features.after_drawed.has_value());
+	EXPECT_FALSE(s.function_features.battlecry.has_value());
+	EXPECT_EQ(s.stat_features.default_cost, 0);
+	EXPECT_EQ(s.stat_features.default_attack, 0);
+	EXPECT_EQ(s.stat_features.default_h_or_d, 0);
 	EXPECT_EQ(s.card_type, CardType::spell);
 }
 
@@ -54,15 +53,15 @@ TEST(WeaponConstructorTest, CardInvariantTest) {
 	stat_features_t sf;
 	sf.default_attack = 3;
 	sf.default_h_or_d = 2;
-	CardInvariant m = CardInvariant(CardType::weapon, ff, sf);
-	EXPECT_FALSE(m.after_played.has_value());
-	EXPECT_FALSE(m.when_played.has_value());
-	EXPECT_FALSE(m.when_drawed.has_value());
-	EXPECT_FALSE(m.after_drawed.has_value());
-	EXPECT_FALSE(m.battlecry.has_value());
-	EXPECT_EQ(m.default_cost, 0);
-	EXPECT_EQ(m.default_attack, 3);
-	EXPECT_EQ(m.default_h_or_d, 2);
-	EXPECT_EQ(m.card_type, CardType::weapon);
+	CardInvariant w = CardInvariant(CardType::weapon, ff, sf);
+	EXPECT_FALSE(w.function_features.after_played.has_value());
+	EXPECT_FALSE(w.function_features.when_played.has_value());
+	EXPECT_FALSE(w.function_features.when_drawed.has_value());
+	EXPECT_FALSE(w.function_features.after_drawed.has_value());
+	EXPECT_FALSE(w.function_features.battlecry.has_value());
+	EXPECT_EQ(w.stat_features.default_cost, 0);
+	EXPECT_EQ(w.stat_features.default_attack, 3);
+	EXPECT_EQ(w.stat_features.default_h_or_d, 2);
+	EXPECT_EQ(w.card_type, CardType::weapon);
 }
 
