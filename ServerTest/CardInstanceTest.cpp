923b6c6fdaa223f3d0b59c7278615c132e1d77a5
gsgyuhwan <isaac9796@naver.com>
2023-10-19 22:13:22 +0900

    Update CardInstanceTest.cpp
    naming fix

--- ServerTest/CardInstanceTest.cpp
+++ ServerTest/CardInstanceTest.cpp
@@ -2,7 +2,7 @@
 
 TEST(DefaultConstructorTest, CardInstanceTest) {
 	CardInstance c = CardInstance();
-	EXPECT_FALSE(c.deathrattle.has_value());
+	EXPECT_FALSE(c.death_rattle.has_value());
 	EXPECT_EQ(c.card_number, 0);
 	EXPECT_EQ(c.cost, 0);
 	EXPECT_EQ(c.attack, 0);
