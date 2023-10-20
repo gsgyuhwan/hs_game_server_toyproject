561f5a21622bb7351020e795e1d6ce579d544b6f
gsgyuhwan <isaac9796@naver.com>
2023-10-20 23:53:29 +0900

    Update Move.h

--- HSGameServer/Move.h
+++ HSGameServer/Move.h
@@ -1,3 +1,102 @@
 #pragma once
 
-class Move;
+enum class MoveType
+{
+	UnaryPlay,
+	BinaryPlay,
+	TernaryPlay,
+	BinaryAttack,
+};
+
+enum class UnaryParam
+{
+	Self,
+	MyHand1 = 1,
+	MyHand2 = 2,
+	MyHand3 = 3,
+	MyHand4 = 4,
+	MyHand5 = 5,
+	MyHand6 = 6,
+	MyHand7 = 7,
+};
+
+enum class BinaryParam
+{
+	Self,
+	Face,
+	AllOpBoard,
+	MyBoard1 = 1,
+	MyBoard2 = 2,
+	MyBoard3 = 3,
+	MyBoard4 = 4,
+	MyBoard5 = 5,
+	MyBoard6 = 6,
+	MyBoard7 = 7,
+	OpBoard1 = 1 << 3,
+	OpBoard2 = 2 << 3,
+	OpBoard3 = 3 << 3,
+	OpBoard4 = 4 << 3,
+	OpBoard5 = 5 << 3,
+	OpBoard6 = 6 << 3,
+	OpBoard7 = 7 << 3,
+	Option1,
+	Option2,
+};
+
+enum class TernaryParam
+{
+	Self,
+	Face,
+	AllOpBoard,
+	MyBoard1 = 1,
+	MyBoard2 = 2,
+	MyBoard3 = 3,
+	MyBoard4 = 4,
+	MyBoard5 = 5,
+	MyBoard6 = 6,
+	MyBoard7 = 7,
+	OpBoard1 = 1 << 3,
+	OpBoard2 = 2 << 3,
+	OpBoard3 = 3 << 3,
+	OpBoard4 = 4 << 3,
+	OpBoard5 = 5 << 3,
+	OpBoard6 = 6 << 3,
+	OpBoard7 = 7 << 3,
+};
+
+
+struct Move 
+{
+	MoveType move_type;
+	UnaryParam unary_param;
+	BinaryParam binary_param;
+	TernaryParam ternary_param;
+
+	int GetMyHandPos() // assert unary_param's value is MyHand1 ~ MyHand7
+	{
+		return static_cast<int>(unary_param);
+	}
+
+	template <typename T> int GetMyBoardPos(){};
+	template<> int GetMyBoardPos<BinaryParam>() // assert binary_param's value is MyBoard1 ~ MyBoard7
+	{
+		return static_cast<int>(binary_param);
+	}
+	template<> int GetMyBoardPos<TernaryParam>() // assert ternary__param's value is MyBoard1 ~ MyBoard7
+	{
+		return static_cast<int>(ternary_param);
+	}
+
+	template <typename T> int GetOpBoardPos() {};
+	template<> int GetOpBoardPos<BinaryParam>() // assert binary_param's value is OpBoard1 ~ OpBoard7
+	{
+		return static_cast<int>(binary_param) >> 3;
+	}
+	template<> int GetOpBoardPos<TernaryParam>() // assert ternary__param's value is OpBoard1 ~ OpBoard7
+	{
+		return static_cast<int>(ternary_param) >> 3;
+	}
+
+
+};
+
