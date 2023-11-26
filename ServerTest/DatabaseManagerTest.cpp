5985c02ba9071a6e4c207cb3a38094ab161d69e2
gsgyuhwan <isaac9796@naver.com>
2023-11-27 04:58:47 +0900

    Update DatabaseManagerTest.cpp

--- ServerTest/DatabaseManagerTest.cpp
+++ ServerTest/DatabaseManagerTest.cpp
@@ -1,15 +1,14 @@
 #include "pch.h"
 
 TEST(GetPlayersDeckTest1, DatabaseManagerTest) {
-	DatabaseManager m = DatabaseManager::Instance();
 	DeckList dl{ 1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15, };
 
-	EXPECT_EQ(m.GetPlayersDeck(1), dl);
+	EXPECT_EQ(DatabaseManager::Instance().GetPlayersDeck(1), dl);
 }
 
 TEST(GetPlayersDeckTest2, DatabaseManagerTest) {
-	DatabaseManager m = DatabaseManager::Instance();
 	DeckList dl{ 1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15, };
 
-	EXPECT_EQ(m.GetPlayersDeck(2), dl);
+	EXPECT_EQ(DatabaseManager::Instance().GetPlayersDeck(2), dl);
 }
+
