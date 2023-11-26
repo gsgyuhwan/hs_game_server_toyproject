9b1f019198b2e86790ec22f5a12d9eb496babf57
gsgyuhwan <isaac9796@naver.com>
2023-11-27 04:58:00 +0900

    Update DatabaseManager.h
    Change implementation of singleton of it

--- HSGameServer/DatabaseManager.h
+++ HSGameServer/DatabaseManager.h
@@ -4,15 +4,24 @@
 class DatabaseManager 
 {
 public:
+	DatabaseManager(const DatabaseManager&) = delete;
+	DatabaseManager(DatabaseManager&&) = delete;
+
+	DatabaseManager& operator=(const DatabaseManager&) = delete;
+	DatabaseManager& operator=(DatabaseManager&&) = delete;
+
 	static DatabaseManager& Instance() {
+		static DatabaseManager instance_;
 		return instance_;
 	}
 
-	DeckList GetPlayersDeck(UserID userid);
-
+	DeckList GetPlayersDeck(UserID userid) {
+		DeckList ret{ 1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15, };
+		return ret;
+	}
 
 private:
-	static DatabaseManager instance_;
+	DatabaseManager() {};
+	~DatabaseManager() {};
 
 };
-DatabaseManager DatabaseManager::instance_;
