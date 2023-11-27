4382fb9e1e5750e84bd5cfcaf1fff520f5c36c08
gsgyuhwan <isaac9796@naver.com>
2023-11-28 03:31:31 +0900

    Update CardInstance.h
    Change types to int and aliased type

--- HSGameServer/CardInstance.h
+++ HSGameServer/CardInstance.h
@@ -1,10 +1,11 @@
 #pragma once
+#include "AliasedTypes.h"
 
 struct CardInstance
 {
-	uint32_t card_number;
-	uint32_t cost;
-	uint32_t attack;
-	uint32_t h_or_d;
+	CardNumber card_number;
+	int cost;
+	int attack;
+	int h_or_d;
 	std::optional<std::function<void(Match&, Move&)>> death_rattle;
 };
