e88c82a3e086039b744285323127a47e9212ad73
gsgyuhwan <isaac9796@naver.com>
2023-11-30 00:33:44 +0900

    Update MatchTest.cpp
    Delete StartMatchTest it's private method

--- ServerTest/MatchTest.cpp
+++ ServerTest/MatchTest.cpp
@@ -2,31 +2,22 @@
 
 class MatchTest : public testing::Test {
 protected:
-	void SetUp() override {
-		boost::asio::io_context io_context1;
-		DummyClient dummy_client1(io_context1, "127.0.0.1", 8888);
-		boost::asio::io_context io_context2;
-		DummyClient dummy_client2(io_service2, "127.0.0.1", 8888);
-		Match match;
-	}
-	void TearDown() override {
+	void SetUp() override 
+	{
 
 	}
+	void TearDown() override 
+	{
 
+	}
+	boost::asio::io_context io_context1;
+	boost::asio::io_context io_context2;
+	DummyClient dummy_client1;
+	DummyClient dummy_client2;
+	Match match;
 };
 
-TEST_F(MatchTest, StartMatchTest) {
-	match.StartMatch();
-	BoardState bs{};
-	EXPECT_EQ(bs, dummy_client1.ReadBoardState());
-	EXPECT_EQ(bs, dummy_client2.ReadBoardState());
-}
 
-TEST_F(MatchTest, EndMatchTest) {
-	match.EndMatch();
-	EXPECT_FALSE(io_context1.has_service());
-	EXPECT_FALSE(io_context2.has_service());
-} 
 
 TEST_F(MatchTest, PlayMoveTest) {
 	Move m{ MoveType::HeroPower};
